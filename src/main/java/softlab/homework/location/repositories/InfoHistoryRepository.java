8a1316bba5178647acc0ea4c4ee3d5aead67952b
MariMgeladze <mgeladze17@gmail.com>
2023-11-19 23:53:23 +0400

    remove files

--- src/main/java/softlab/homework/location/repositories/InfoHistoryRepository.java
+++
@@ -1,6 +0,0 @@
-package softlab.homework.location.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InfoHistoryRepository extends JpaRepository<InfoHistoryRepository,Integer> {
-}
