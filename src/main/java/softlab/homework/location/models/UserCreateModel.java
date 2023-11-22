2db7110bb31799f4093bb493aca9f825cb67a070
MariMgeladze <mgeladze17@gmail.com>
2023-11-22 21:14:07 +0400

    add user files

--- src/main/java/softlab/homework/location/models/UserCreateModel.java
+++ src/main/java/softlab/homework/location/models/UserCreateModel.java
@@ -1,8 +1,5 @@
 package softlab.homework.location.models;
 
-import java.time.LocalDateTime;
-
-
 public record UserCreateModel( String username, String password, String firstName,String lastName){
 
 }
